95b44c28debe1cb2c349dc3597f8aff64b892793
SumairShaukat <sumairch224@gmail.com>
2023-01-22 22:59:58 +0500

    tab-tabs-tabs-panel and select atom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Buttonatom from "../src/atoms/Button/index";
 import SwitchAtom from "./atoms/switch/index";
 import Modal from "./atoms/modal/index";
+import Badges from "./atoms/Badge/index";
+
 function App() {
   return (
     <div className="App">
       <Buttonatom />
       <SwitchAtom />
-      <Modal />
     </div>
   );
 }
